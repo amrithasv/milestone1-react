56792cdfd150a3f4def68bd2f7545a1cda0aeaec
amrithasv <svamritha93@gmail.com>
2023-07-24 17:27:18 +0530

    commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+
+import Navbar from './Components/Navbar';
+import Home from './Components/breadHome';
+import Menu from './Components/breadMenu';
+import About from './Components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container mt-4">
+          <Route exact path="/" component={Home} />
+          <Route path="/menu" component={Menu} />
+          <Route path="/about" component={About} />
+        </div>
+        <footer className="footer mt-auto py-3 bg-light">
+          <div className="container text-center">
+            © {new Date().getFullYear()} FoodCo. All rights reserved.
+          </div>
+        </footer>
+      </div>
+    </Router>
   );
 }
 
